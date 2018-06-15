59edbc306bed4728d2aebb73029d7d3aa573989a
Krishna <msarath590@gmail.com>
2018-06-15 20:58:26 +0530

    Hover added for material menu

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
+import { NavbarTopmenuComponent } from './navbar-topmenu/navbar-topmenu.component';
 import { AngularFontAwesomeModule } from 'angular-font-awesome';
 import { AppRoutingModule } from '../app-routing.module';
 import { SharedModule} from 'diva-shared-apps/shared.module';
+import {MatButtonModule, MatToolbarModule} from '@angular/material';
+import { MatMenuModule } from '@angular/material/menu';
 
 
 @NgModule({
@@ -11,9 +14,11 @@ import { SharedModule} from 'diva-shared-apps/shared.module';
     CommonModule,
     AngularFontAwesomeModule,
     AppRoutingModule,
-    SharedModule
+    SharedModule,MatButtonModule,MatToolbarModule,MatMenuModule
   ],
-  declarations: [NavbarComponent],
-  exports: [NavbarComponent]
+  declarations: [NavbarComponent,
+  NavbarTopmenuComponent],
+  exports: [NavbarComponent,
+  NavbarTopmenuComponent]
 })
 export class CoreModule { }
